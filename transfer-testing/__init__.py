baf52de2a615757fd52a5534b68a7002d277721c
Szymon Zaborowski <zabor2432@gmail.com>
2023-07-13 22:34:43 +0200

    fix: remove files

--- transfer-testing/__init__.py
+++
@@ -1,9 +0,0 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-
-if __name__ == "__main__":
-    # TODO: argparse for different datasets
-    api = KaggleApi()
-    api.authenticate()
-
-    api.competition_download_file("tiny-imagenet", path="./data")
